7bf67c7b204f418b67a0afd4bc5848e83837d8a7
Prathamesh Patil <122prathamesh6002@sjcem.edu.in>
2026-05-24 09:28:45 +0530

    Next js repo init

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from users.views import LogOutView, LoginView, RefreshView, RegisterView
-
-urlpatterns=[
-    path("login/", LoginView.as_view(), name="login"),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("refresh/", RefreshView.as_view(), name="refresh-token"),
-    path("logout/", LogOutView.as_view(), name="logout")
-]
